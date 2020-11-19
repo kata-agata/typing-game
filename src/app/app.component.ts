7411c524b82d54e754232afd69475d61e93cd25f
kata-agata <agata.k.jazdzewska@gmail.com>
2020-11-19 10:50:46 +0100

    working app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from "faker";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'typing-game';
+  randomSentence = lorem.sentence();
+  inputSentence = '';
+
+  onInputChange(value: string){
+    this.inputSentence = value;
+  }
+
+  compareLetters(randomLetter: string, inputLetter: string){
+    if(!inputLetter){ // if inputLetter is undefined
+      return '';
+    }
+    // if(randomLetter === inputLetter){
+    //   return "correct"
+    // }
+    // else {
+    //   return "wrong"
+    // }
+    return randomLetter === inputLetter ? 'correct' : 'wrong';
+  }
 }
